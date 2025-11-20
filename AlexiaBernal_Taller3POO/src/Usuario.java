5b40fd91054547b5d04f88eac2b758bcfd93bc6f
Alexia <alexia.bernal@alumnos.ucn.cl>
2025-11-20 20:39:19 -0300

    clase usuario

--- AlexiaBernal_Taller3POO/src/Usuario.java
+++ AlexiaBernal_Taller3POO/src/Usuario.java
@@ -1,4 +1,27 @@
 
-public class Usuario {
-
+public class Usuario 
+{
+	String nombre;
+	String contraseña;
+	String rol;
+	public Usuario(String nombre, String contraseña, String rol) 
+	{
+		super();
+		this.nombre = nombre;
+		this.contraseña = contraseña;
+		this.rol = rol;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public String getRol() {
+		return rol;
+	}
+	
+	
+	
+	
 }
